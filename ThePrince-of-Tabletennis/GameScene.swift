c47dade3db8e8967ca3af6b8bef8bbf3031e2a61
slowhand0309 <slowhand0309@gmail.com>
2016-06-05 00:59:15 +0900

    Added racket and ball image

--- ThePrince-of-Tabletennis/GameScene.swift
+++ ThePrince-of-Tabletennis/GameScene.swift
@@ -18,6 +18,7 @@ class GameScene: SKScene {
     override init(size: CGSize) {
         super.init(size: size)
         drawBackground()
+        drawRacket()
     }
     
     // Draw background color.
@@ -25,6 +26,14 @@ class GameScene: SKScene {
         let box = SKSpriteNode(color: SKColor.blueColor(), size: self.size)
         box.position = CGPointMake(self.size.width / 2, self.size.height / 2)
         self.addChild(box)
+    }
+    
+    // Draw racket image.
+    private func drawRacket() {
+        let image = SKSpriteNode(imageNamed: "racket.png")
+        image.size = self.size
+        image.position = CGPointMake(self.frame.size.width / 2, self.frame.size.height / 2)
+        self.addChild(image)
     }
     
     override func didMoveToView(view: SKView) {
